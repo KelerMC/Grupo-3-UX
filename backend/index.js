3ac8c4c918ac40f9fa1e3ac295583e34007e1328
wguia03 <willgm.2003@gmail.com>
2024-01-18 23:24:38 -0500

    manejo de cors

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,18 @@ app.use(express.json());
 app.use("/estudiantes", estRouter);
 app.use("/profesores", profRoutes);
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://g5-ihcproyecto1.onrender.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
 });
