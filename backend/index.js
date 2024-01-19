1a8f46ee062ea86cf6c6abb73750eb8755d67f7c
wguia03 <willgm.2003@gmail.com>
2024-01-18 23:41:52 -0500

    arreglando cors

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,17 @@
 require("dotenv").config();
 require("./src/config/database");
 const express = require("express");
+const cors = require("cors");
 const estRouter = require("./src/routers/estRoutes");
 const profRoutes = require("./src/routers/profRoutes");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use("/estudiantes", estRouter);
 app.use("/profesores", profRoutes);
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://g5-ihcproyecto1.onrender.com"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
