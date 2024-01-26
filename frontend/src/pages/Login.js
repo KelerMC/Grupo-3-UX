54accaf640bfd0f6a7f031aa83f8fd9e1ab1ff81
Joaquin Hidalgo <joaquin.hidalgo@unmsm.edu.pe>
2024-01-25 21:19:36 -0500

    css corregido login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,42 +3,35 @@ import { Link } from 'react-router-dom';
 import logo from '../img/logo.png';
 
 export default function Login() {
-
   return (
-    
-    <div className="fondo-pantalla"> 
-    <div className="logo">
-    <img src={logo} alt="logo" />
-    </div>
-    
+    <div className="fondo-pantalla">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+
       <div className="contenedor-principal">
-      <div className="titulo-LoginPrincipal">
-      <h1>Bienvenido al Sistema de la Universidad</h1>
-      </div>
-    </div>
-      
-      
-      <div className="contenedor-profesor">
-        <div className="titulo-profesores">
-          <h2>Profesores</h2>
-          <Link to="/login-profesor">
-          <button>Iniciar Sesión</button>
-        </Link>
-        </div>
-        
-        
+        <h1>Bienvenido al Sistema de la Universidad</h1>
       </div>
 
-      <div className="contenedor-estudiante">
-      <div className="titulo-estudiante">
-          <h2>Estudiantes</h2>
-          <Link to="/login-estudiante">
-          <button>Iniciar Sesión</button>
-        </Link>
+      <div className="contenedor-horizontal">
+        <div className="contenedor-profesor">
+          <div className="titulo-profesores">
+            <h2>Profesores</h2>
+            <Link to="/login-profesor">
+              <button className="titulo-profesores-button">Iniciar Sesión</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="contenedor-estudiante">
+          <div className="titulo-estudiante">
+            <h2>Estudiantes</h2>
+            <Link to="/login-estudiante">
+              <button className="titulo-estudiante-button">Iniciar Sesión</button>
+            </Link>
+          </div>
         </div>
       </div>
-
-      {/* Puedes agregar más elementos o estilos según tus necesidades */}
     </div>
   );
 }
