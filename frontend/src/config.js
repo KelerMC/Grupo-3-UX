57b6b961f9690d21a56fae340dfdac254c942b24
Joaquin Hidalgo <joaquin.hidalgo@unmsm.edu.pe>
2024-02-09 21:38:55 -0500

    xd

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-//export const API_URL = 'https://ihc-proyecto01.onrender.com'
-export const API_URL = 'http://localhost:8081'
+//export const API_URL = 'https://ihc-proyecto01.onrender.com' no relacional
+//export const API_URL = 'http://localhost:8081'
+export const API_URL ='https://backendmatricula.onrender.com'
