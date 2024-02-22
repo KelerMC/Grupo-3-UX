078052ac67a592dd19adab87704971f25d2b8739
Joaquin Hidalgo <joaquin.hidalgo@unmsm.edu.pe>
2024-02-22 14:20:29 -0500

    exhoneracion

--- frontend/src/pages/VozProbar.js
+++ frontend/src/pages/VozProbar.js
@@ -1,42 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import '../styles/VozProbar.css';
  
-const VozProbar = () => {
-  const { transcript, resetTranscript } = useSpeechRecognition();
+const VozProbar = () => {  
   const [isListening, setIsListening] = useState(false);
 
-  useEffect(() => {
-    console.log("Navegador compatible con reconocimiento de voz:", SpeechRecognition.browserSupportsSpeechRecognition());
-  }, []);
-
-  useEffect(() => {
-    console.log("Reconocimiento de voz activo:", isListening);
-  }, [isListening]);
-
-  const startListening = () => {
-    SpeechRecognition.startListening({
-      language: 'es-ES' // Especifica el idioma español (España)
-    });
-    setIsListening(true);
-  };
-
-  const stopListening = () => {
-    SpeechRecognition.stopListening();
-    setIsListening(false);
-  };
-
-  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    return <span>La navegación por voz no es compatible con tu navegador.</span>;
-  }
-
+  
   return (
     <div className="container"> 
-      <div className="voice-container">
-        <p>Última transcripción: {transcript}</p>
-        <button onClick={startListening}>Iniciar reconocimiento de voz</button>
-        <button onClick={stopListening}>Detener reconocimiento de voz</button>
-        <button onClick={resetTranscript}>Reiniciar transcripción</button>
+      <div className="voice-container">        
       </div>
     </div>
   );
