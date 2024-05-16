9985af1c7ce7d237e85e801ecf0e5dfe1b591b7a
KelerMC <keler.modesto@unmsm.edu.pe>
2024-05-15 22:34:09 -0500

    Configuración back-front

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-//export const API_URL = 'https://ihc-proyecto01.onrender.com' no relacional
-//export const API_URL = 'http://localhost:8081'
-export const API_URL ='https://backendmatricula.onrender.com'
+
+//export const API_URL = 'http://localhost:8080'
+export const API_URL ='https://grupo-3-ux.onrender.com'
